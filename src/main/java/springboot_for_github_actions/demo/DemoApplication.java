442f861e23186592a8193843dd107fa3d756de9b
emingordon <emin.xazehaqq@gmail.com>
2025-09-15 23:09:12 +0000

    Add HelloController and web dependency

--- src/main/java/springboot_for_github_actions/demo/DemoApplication.java
+++ src/main/java/springboot_for_github_actions/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package springboot_for_github_actions.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+@RestController
+class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
 }

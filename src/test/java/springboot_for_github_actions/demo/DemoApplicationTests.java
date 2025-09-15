82e81645ebad6126b129071397e2267abd2b97e6
emingordon <emin.xazehaqq@gmail.com>
2025-09-15 22:46:15 +0000

    Initial commit: Spring Boot demo app

---
+++ src/test/java/springboot_for_github_actions/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_for_github_actions.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
